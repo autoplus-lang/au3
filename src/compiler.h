d389ad76496ee10cbccf9146b5c6f85f0c545f02
wy3 <wuuyi123@gmail.com>
2019-12-26 07:28:37 +0700

    implement lexer

--- src/compiler.h
+++ src/compiler.h
@@ -2,5 +2,43 @@
 #define _AU3_COMPILER_H
 #pragma once
 
+#include "common.h"
+#include "chunk.h"
+
+typedef enum {
+    // Single-character tokens.                         
+    TOKEN_LEFT_PAREN, TOKEN_RIGHT_PAREN,
+    TOKEN_LEFT_BRACE, TOKEN_RIGHT_BRACE,
+    TOKEN_COMMA, TOKEN_DOT, TOKEN_MINUS, TOKEN_PLUS,
+    TOKEN_SEMICOLON, TOKEN_SLASH, TOKEN_STAR,
+
+    // One or two character tokens.                     
+    TOKEN_BANG, TOKEN_BANG_EQUAL,
+    TOKEN_EQUAL, TOKEN_EQUAL_EQUAL,
+    TOKEN_GREATER, TOKEN_GREATER_EQUAL,
+    TOKEN_LESS, TOKEN_LESS_EQUAL,
+
+    // Literals.                                        
+    TOKEN_IDENTIFIER, TOKEN_STRING, TOKEN_NUMBER,
+
+    // Keywords.                                        
+    TOKEN_AND, TOKEN_CLASS, TOKEN_ELSE, TOKEN_FALSE,
+    TOKEN_FOR, TOKEN_FUN, TOKEN_IF, TOKEN_NIL, TOKEN_OR,
+    TOKEN_PRINT, TOKEN_RETURN, TOKEN_SUPER, TOKEN_THIS,
+    TOKEN_TRUE, TOKEN_VAR, TOKEN_WHILE,
+
+    TOKEN_ERROR,
+    TOKEN_EOF
+} au3TokenType;
+
+typedef struct {
+    au3TokenType type;
+    const char *start;
+    int length;
+    int line;
+} au3Token;
+
+void au3_initLexer(const char *source);
+au3Token au3_scanToken();
 
 #endif
