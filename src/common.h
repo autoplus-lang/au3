84314b6945d38524d5811be7a32b864490c042af
wy3 <wuuyi123@gmail.com>
2019-12-26 21:46:09 +0700

    add combination bytes macros

--- src/common.h
+++ src/common.h
@@ -10,10 +10,14 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#define AU3_MAX_CONSTS  256
-#define AU3_MAX_LOCALS  256
-#define AU3_MAX_ARGS    32
-#define AU3_MAX_FRAMES  64
-#define AU3_MAX_STACK   (AU3_MAX_FRAMES * AU3_MAX_LOCALS)
+#define AU3_COMBINE(l, r)   (uint8_t)(((char)(l) & 0xF) | ((char)(r) & 0xF) << 4)
+#define AUP_COMBINE_L(c)    (char)(((c) & 0xF))
+#define AUP_COMBINE_R(c)    (char)((((c) >> 4) & 0xF))
+
+#define AU3_MAX_CONSTS      256
+#define AU3_MAX_LOCALS      256
+#define AU3_MAX_ARGS        32
+#define AU3_MAX_FRAMES      64
+#define AU3_MAX_STACK       (AU3_MAX_FRAMES * AU3_MAX_LOCALS)
 
 #endif
